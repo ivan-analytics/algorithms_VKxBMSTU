8c770399fce31d52d8138f66e173ff4a42459702
Penguin-Run <penguinlgb@gmail.com>
2020-10-31 00:07:02 +0300

    added templates

--- task_5/main.cpp
+++ task_5/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cassert>
+#include <cstring>
 
 /*
  *
@@ -9,7 +11,8 @@
  - Копируем остаток второго массива в конец массива-результата
  */
 
-void merge(const int* arr_first, int len_first, const int* arr_second, int len_second, int* c) {
+template<class T>
+void merge(const std::pair<T, int>* arr_first, int len_first, const std::pair<T, int>* arr_second, int len_second, std::pair<T, int>* c) {
     int ind_first = 0;
     int ind_second = 0;
     int ind_c = 0;
@@ -35,7 +38,7 @@ void merge(const int* arr_first, int len_first, const int* arr_second, int len_s
 
         // выберем массив, крайний элемент которого меньше
         // извлечем этот элемент в массив-результат
-        if (arr_first[ind_first] <= arr_second[ind_second]) {
+        if (arr_first[ind_first].first <= arr_second[ind_second].first) {
             c[ind_c] = arr_first[ind_first];
             ind_first++;
             ind_c++;
@@ -48,7 +51,8 @@ void merge(const int* arr_first, int len_first, const int* arr_second, int len_s
     }
 }
 
-void MergeSort(int* a, int aLen ) {
+template<class T>
+void MergeSort(std::pair<T, int>* a, int aLen ) {
     if( aLen <= 1 ) {
         return;
     }
@@ -56,25 +60,58 @@ void MergeSort(int* a, int aLen ) {
     int secondLen = aLen - firstLen;
     MergeSort( a, firstLen );
     MergeSort( a + firstLen, secondLen );
-    int* c = new int[aLen];
+    auto* c = new std::pair<T, int>[aLen];
     merge(a, firstLen, a + firstLen, secondLen, c);
-    memcpy( a, c, sizeof( int ) * aLen );
+    memcpy( a, c, sizeof( std::pair<T, int> ) * aLen );
     delete[] c;
 }
 
+#define UP 23
+#define DOWN 32
+
+template<class T>
+void run(std::pair<T, int>* arr, size_t num_of_elements) {
+
+    MergeSort(arr, num_of_elements);
+
+    int level = 0;
+    int counter = 0;
+    for (int i = 0; i < num_of_elements; i++) {
+        if (level > 0) {
+            assert(i != 0);
+            assert((arr[i].first - arr[i-1].first) >= 0);
+            counter += arr[i].first - arr[i-1].first;
+        }
+
+        if (arr[i].second == UP)
+            level++;
+        else if (arr[i].second == DOWN)
+            level--;
+        else
+            assert(false);
+        assert(level >= 0);
+    }
+
+    std::cout << counter;
+}
+
 int main() {
-
-    int n;
-    std::cin >> n;
-    int* arr = (int*)(malloc(n * sizeof(int)));
-    for (int i = 0; i < n; i++) std::cin >> arr[i];
-    for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
-    MergeSort(arr, n);
-
-    std::cout << std::endl;
-    for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
+    // input
+    size_t N = 0;
+    std::cin >> N;
+    size_t num_of_elements = N * 2;
+    auto* arr = (std::pair<int, int>*)(malloc(num_of_elements * sizeof(std::pair<int, int>)));
+    for (int i = 0; i < num_of_elements; i++) {
+        std::cin >> arr[i].first;
+        if (i % 2 == 0)
+            arr[i].second = UP;
+        else
+            arr[i].second = DOWN;
+    }
     
+    run(arr, num_of_elements);
 
+    delete[] arr;
     return 0;
 }
 
