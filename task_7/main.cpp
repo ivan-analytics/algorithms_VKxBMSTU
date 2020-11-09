9411b07b5fb4d3a9584302f15768661801254d76
Penguin-Run <penguinlgb@gmail.com>
2020-11-09 19:26:35 +0300

    basic str sort

--- task_7/main.cpp
+++ task_7/main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 
 // TODO: implement 256 alphabet
-void counting_sort(int* a, int n, int k) {
+int get_digit(std::string& str, int digit) {
+    return (int) str[digit];
+}
+
+std::string* counting_sort(std::string* a, int n, int k, int digit) {
     int* c = new int[k];
     for( int i = 0; i < k; ++i )
         c[i] = 0;
     for( int i = 0; i < n; ++i )
-        ++c[a[i]];
+        ++c[get_digit(a[i], digit)];
     int sum = 0;
     for( int i = 0; i < k; ++i ) {
         int tmp = c[i];
         c[i] = sum; // Начала групп.
         sum += tmp;
     }
-    int* b = new int[n];
+    auto* b = new std::string[n];
     for( int i = 0; i < n; ++i ) {
-        b[c[a[i]]++] = a[i];
+        b[c[get_digit(a[i], digit)]++] = a[i];
     }
     delete[] c;
-    memcpy( a, b, n * sizeof( int ) );
-    delete[] b;
+    // memcpy( a, b, n * sizeof( int ) );
+    return b;
 }
 
 void get_lines() {
@@ -27,13 +31,19 @@ void get_lines() {
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    int* a = new int[n];
-    for (int i = 0; i < n; i++) std::cin >> a[i];
+    size_t num_of_str  = 5;
+    std::string str[num_of_str];
+    for (int i = 0; i < num_of_str; i++)
+        std::getline(std::cin, str[i]);
 
-    counting_sort(a, n, 10);
+    std::string* res = counting_sort(str, num_of_str, 128, 0);
 
-    for (int i = 0; i < n; i++) std::cout << a[i] << " ";
+    for (int i = 0; i < num_of_str; i++)
+        std::cout << res[i] << std::endl;
     return 0;
 }
+
+
+/*
+
+ */
